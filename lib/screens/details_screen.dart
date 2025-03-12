aea8e12bf95fabe3b9fec97507ae16e7d5a0bd38
Daniil Fadeev <dafadeev_1@edu.hse.ru>
2025-03-12 18:49:59 +0300

    upd: шрифты поменял

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -20,95 +20,174 @@ class DetailsScreen extends StatelessWidget {
     final int dogFriendly = args?['dogFriendly'] ?? 0;
     final int sheddingLevel = args?['sheddingLevel'] ?? 0;
     final bool hypoallergenic = args?['hypoallergenic'] == 1;
-    final String wikipediaUrl = args?['wikipediaUrl'] ?? '';
 
     return Scaffold(
-      appBar: AppBar(title: Text(breedName)),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            imageUrl.isNotEmpty
-                ? Image.network(imageUrl, height: 300)
-                : Text('Ошибка загрузки изображения'),
-            SizedBox(height: 20),
-            Text(
-              'Страна: $origin',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Продолжительность жизни: $lifeSpan лет',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Характер: $temperament',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Описание: $description',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Дополнительные характеристики:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            _buildCharacteristic('Энергичность', energyLevel),
-            _buildCharacteristic('Интеллект', intelligence),
-            _buildCharacteristic('Дружелюбность к детям', childFriendly),
-            _buildCharacteristic('Дружелюбность к собакам', dogFriendly),
-            _buildCharacteristic('Уровень линьки', sheddingLevel),
-            SizedBox(height: 10),
-            Text(
-              hypoallergenic ? '✅ Гипоаллергенный' : '❌ Не гипоаллергенный',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            if (wikipediaUrl.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: TextButton(
-                  onPressed: () => _openWikipedia(wikipediaUrl, context),
-                  child: Text(
-                    'Подробнее на Wikipedia',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 350,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Hero(
+                tag: imageUrl,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    imageUrl.isNotEmpty
+                        ? Image.network(imageUrl, fit: BoxFit.cover)
+                        : Container(color: Colors.grey),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        color: Colors.black.withValues(alpha: 0.4),
+                      ),
+                    ),
+                    Positioned(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          breedName,
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-          ],
-        ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _buildInfoText(
+                    'Страна: $origin',
+                    fontSize: 18,
+                    isBold: true,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10),
+                  _buildInfoText(
+                    'Продолжительность жизни: $lifeSpan лет',
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 10),
+                  _buildInfoText(
+                    'Характер: $temperament',
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 10),
+                  _buildInfoText(
+                    'Описание: $description',
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 20),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          _buildSectionTitle('Дополнительные характеристики:'),
+                          SizedBox(height: 10),
+                          _buildCharacteristic('Энергичность', energyLevel),
+                          _buildCharacteristic('Интеллект', intelligence),
+                          _buildCharacteristic(
+                            'Дружелюбность к детям',
+                            childFriendly,
+                          ),
+                          _buildCharacteristic(
+                            'Дружелюбность к собакам',
+                            dogFriendly,
+                          ),
+                          _buildCharacteristic('Уровень линьки', sheddingLevel),
+                          SizedBox(height: 10),
+                          _buildInfoText(
+                            hypoallergenic
+                                ? '✅ Гипоаллергенный'
+                                : '❌ Не гипоаллергенный',
+                            isBold: true,
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildInfoText(
+    String text, {
+    double fontSize = 16,
+    bool isBold = false,
+    TextAlign textAlign = TextAlign.start,
+  }) {
+    return Text(
+      text,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+        fontFamily: 'Montserrat',
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Montserrat',
       ),
     );
   }
 
   Widget _buildCharacteristic(String title, int level) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          '$title: ',
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        Row(
-          children: List.generate(
-            level,
-            (index) => Icon(Icons.star, color: Colors.amber, size: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '$title: ',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat',
+            ),
           ),
-        ),
-      ],
+          Row(
+            children: List.generate(
+              level,
+              (index) => Icon(Icons.star, color: Colors.amber, size: 20),
+            ),
+          ),
+        ],
+      ),
     );
-  }
-
-  void _openWikipedia(String url, BuildContext context) {
-    // TODO: сделать редирект на браузер
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(SnackBar(content: Text('Открытие Wikipedia: $url')));
   }
 }
